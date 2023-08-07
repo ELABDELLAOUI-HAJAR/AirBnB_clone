7213d0c687098e19b900b5f2f1cfb15303895771
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-08-07 09:53:45 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,23 @@ class BaseModel:
     """Class BaseModel defines all common
     attributes/methods for other classes
     """
-    def __init__(self):
-        """Initialise BaseModel attributes"""
+    def __init__(self, *args, **kwargs):
+        """Initialise BaseModel attributes
+        Arguments:
+        args: Not used
+        kwargs: Dictionary representation of BaseModel instance
+        """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            elif key == "created_at":
+                self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            elif key == "updated_at":
+                self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """Return string representation of BaseModel"""
