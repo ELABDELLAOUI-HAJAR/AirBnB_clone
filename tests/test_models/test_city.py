a37f823b2c26b00c7dac00af423ce5b45de1233d
HAJAR <elabdellaoui.hajar@gmail.com>
2023-08-09 11:48:09 +0000

    Add unittests for City module

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,55 @@
+"""This module contains tests for the City Class"""
+from unittest import TestCase, main
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+import uuid
+
+
+class TestCity(TestCase):
+    """Definition of Test Class of City"""
+
+    def test_city_inherits_base_model(self):
+        """test_city_inherits_base_model"""
+        self.assertTrue(issubclass(City, BaseModel))
+
+    def test_city_with_valid_id(self):
+        """test_city_with_valid_id"""
+        city = City()
+        self.assertEqual(str(uuid.UUID(city.id)), city.id)
+
+    def test_city_check_attributes(self):
+        """test_city_check_attributes"""
+        city = City()
+        self.assertTrue(hasattr(city, "id"))
+        self.assertTrue(hasattr(city, "created_at"))
+        self.assertTrue(hasattr(city, "updated_at"))
+        self.assertTrue(hasattr(city, "name"))
+        self.assertTrue(hasattr(city, "state_id"))
+
+    def test_city_set_name(self):
+        """test_city_set_name"""
+        city = City()
+        city.name = "Marrakech"
+        self.assertEqual(city.name, "Marrakech")
+
+    def test_city_set_state_id(self):
+        """test_city_set_name"""
+        city = City()
+        state = State()
+        city.state_id = state.id
+        self.assertEqual(city.state_id, state.id)
+
+    def test_city_to_dict(self):
+        """test_city_to_dict"""
+        city = City()
+        city_dict = {
+                "__class__": "City",
+                "id": city.id,
+                "created_at": city.created_at.isoformat(),
+                "updated_at": city.updated_at.isoformat()
+                }
+        self.assertDictEqual(city_dict, city.to_dict())
+        city_dict["name"] = "Marrakech"
+        city.name = "Marrakech"
+        self.assertDictEqual(city_dict, city.to_dict())
